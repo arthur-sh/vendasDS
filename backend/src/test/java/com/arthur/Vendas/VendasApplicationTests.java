4028b5c83b95ac1af5036338db41d66ef5f389c2
Arthur Teixeira <arthureanalaura@gmail.com>
2021-05-03 20:49:38 -0300

    Project created

---
+++ backend/src/test/java/com/arthur/Vendas/VendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthur.Vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
