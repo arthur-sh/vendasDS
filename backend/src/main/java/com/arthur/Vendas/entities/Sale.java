b75011554c0f4d4a29d3d7320be42f391b4b3981
Arthur Teixeira <arthureanalaura@gmail.com>
2021-05-05 13:18:42 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/arthur/Vendas/entities/Sale.java
@@ -0,0 +1,35 @@
+package com.arthur.Vendas.entities;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_sales")
+public class Sale {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer visited;
+    private Integer deals;
+    private Double amount;
+    private LocalDate date;
+
+    @ManyToOne
+    @JoinColumn(name = "seller_id")
+    private Seller seller;
+
+    public Sale(){
+    }
+
+    public Sale(Long id, Integer visited, Integer deals, Double amount, LocalDate date, Seller seller) {
+        this.id = id;
+        this.visited = visited;
+        this.deals = deals;
+        this.amount = amount;
+        this.date = date;
+        this.seller = seller;
+    }
+
+
+}
