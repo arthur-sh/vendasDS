70ab6d70f3866436b577ad5102ef8a0adb37e166
Arthur Teixeira <arthureanalaura@gmail.com>
2021-05-08 19:09:25 -0300

    Pagination

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,3 +1,9 @@
-export type SaleSum = {sellerName: string; sum: number}
+import { Seller } from "./sellers"
 
-export type SaleSuccess = {sellerName: string; visited: number; deals: number}
+export type Sale = { id: number; visited: number; deals: number; amount: number; date: string; seller: Seller }
+
+export type SalePage = { content?: Sale[]; last: boolean; totalPages: number; totalElements: number; first: boolean; number: number; numberOfElements?: number; size?: number; empty?: boolean }
+
+export type SaleSum = { sellerName: string; sum: number }
+
+export type SaleSuccess = { sellerName: string; visited: number; deals: number }
